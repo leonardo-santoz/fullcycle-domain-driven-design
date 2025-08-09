be3d502031c2a06c6289d5290cc515c267d328b3
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2025-08-08 23:32:22 -0300

    feat: entidades - autovalidation principe

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -8,13 +8,30 @@ class Customer {
     this._id = id;
     this._name = name;
     this._address = address;
+    this.validate();
+  }
+
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._address.length === 0) {
+      throw new Error("Address is required");
+    }
   }
 
   changeName(name: string) {
     this._name = name;
+    this.validate();
   }
 
   activate() {
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
     this._active = true;
   }
 
