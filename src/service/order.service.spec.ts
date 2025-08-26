c2ba4184b8f189fc676560ca9649eb97315563c7
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2025-08-25 21:52:26 -0300

    feat: Domain Services - implementando programa de rewards

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -10,7 +10,7 @@ describe("Order service unit tests", () => {
 
     const order = OrderService.placeOrder(customer, [item1]);
 
-    expect(customer.rewardPonts).toBe(5);
+    expect(customer.rewardPoints).toBe(5);
     expect(order.total()).toBe(10);
   });
 
--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,6 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
+
+import Customer from "../entity/customer";
 import Order from "../entity/order";
+import OrderItem from "../entity/order_item";
 
 export default class OrderService {
+  static placeOrder(customer: Customer, items: OrderItem[]): Order {
+    if (items.length === 0) {
+      throw new Error("Order must have at least one item");
+    }
+
+    const order = new Order(uuidv4(), customer.id, items);
+    customer.addRewardPoints(order.total() / 2);
+
+    return order;
+  }
+
   static total(orders: Order[]): number {
     return orders.reduce((acc, order) => acc + order.total(), 0);
   }
