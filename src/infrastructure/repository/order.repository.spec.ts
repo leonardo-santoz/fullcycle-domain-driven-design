344fc9adba2db85499a3c62ce050315ca87fedf7
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2025-09-17 19:50:15 -0300

    feat: Repositories - modelando Order e Items

---
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize-typescript";
+
+import Address from "../../domain/entity/address";
+import Customer from "../../domain/entity/customer";
+import CustomerModel from "../db/sequelize/model/customer.model";
+import CustomerRepository from "./customer.repository";
+
+describe("Order repository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+});
