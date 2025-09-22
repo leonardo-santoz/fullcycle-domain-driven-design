3d3183ec5a93385f7ee795de3f1835e75a9eb8eb
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2025-09-22 18:33:52 -0300

    feat: Repositories - add order repository update com teste unitário

--- src/infrastructure/repository/order.repository.spec.ts
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -84,4 +84,97 @@ describe("Order repository test", () => {
       ],
     });
   });
+
+  it("should update an order", async () => {
+    const customerRepository = new CustomerRepository();
+    const customer = new Customer("123", "Customer 1");
+    const address = new Address("Street 1", 1, "Zipcode 1", "City 1");
+    customer.changeAddress(address);
+    await customerRepository.create(customer);
+
+    const productRepository = new ProductRepository();
+    const product = new Product("123", "Product 1", 10);
+    await productRepository.create(product);
+
+    const orderItem = new OrderItem(
+      "1",
+      product.name,
+      product.price,
+      product.id,
+      2
+    );
+
+    const order = new Order("123", customer.id, [orderItem]);
+
+    const orderRepository = new OrderRepository();
+    await orderRepository.create(order);
+
+    const orderModel = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ["items"],
+    });
+
+    expect(orderModel.toJSON()).toStrictEqual({
+      id: "123",
+      customer_id: "123",
+      total: order.total(),
+      items: [
+        {
+          id: orderItem.id,
+          name: orderItem.name,
+          price: orderItem.price,
+          quantity: orderItem.quantity,
+          product_id: "123",
+          order_id: "123",
+        },
+      ],
+    });
+
+    const product2 = new Product("456", "Product 2", 15);
+    await productRepository.create(product2);
+
+    const orderItem2 = new OrderItem(
+      "2",
+      product2.name,
+      product2.price,
+      product2.id,
+      1
+    );
+
+    const orderToUpdate = new Order("123", customer.id, [
+      orderItem,
+      orderItem2,
+    ]);
+
+    await orderRepository.update(orderToUpdate);
+
+    const orderModel2 = await OrderModel.findOne({
+      where: { id: order.id },
+      include: ["items"],
+    });
+
+    expect(orderModel2.toJSON()).toStrictEqual({
+      id: "123",
+      customer_id: "123",
+      total: orderToUpdate.total(),
+      items: [
+        {
+          id: orderItem.id,
+          name: orderItem.name,
+          price: orderItem.price,
+          quantity: orderItem.quantity,
+          product_id: "123",
+          order_id: "123",
+        },
+        {
+          id: orderItem2.id,
+          name: orderItem2.name,
+          price: orderItem2.price,
+          quantity: orderItem2.quantity,
+          product_id: "456",
+          order_id: "123",
+        },
+      ],
+    });
+  });
 });
--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,8 +1,10 @@
 import Order from "../../domain/entity/order";
 import OrderModel from "../db/sequelize/model/order.model";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderRepositoryInterface from "../../domain/repository/order-repository.interface";
+import { Op, where } from "sequelize";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
@@ -22,5 +24,51 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel, as: "items" }],
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    await OrderModel.sequelize.transaction(async (transaction) => {
+      await OrderModel.update(
+        { customer_id: entity.customerId, total: entity.total() },
+        { where: { id: entity.id }, transaction }
+      );
+
+      const currentItems = entity.items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        product_id: item.productId,
+        quantity: item.quantity,
+        order_id: entity.id,
+      }));
+
+      const currentItemsIds = entity.items.map((item) => item.id);
+
+      await OrderItemModel.destroy({
+        where: {
+          order_id: entity.id,
+          id: { [Op.notIn]: currentItemsIds },
+        },
+      });
+
+      await OrderItemModel.bulkCreate(currentItems, {
+        updateOnDuplicate: [
+          "name",
+          "price",
+          "product_id",
+          "quantity",
+          "order_id",
+        ],
+        transaction,
+      });
+    });
+  }
+
+  async find(id: string): Promise<Order> {
+    throw new Error("method not implemented");
+  }
+
+  async findAll(): Promise<Order[]> {
+    throw new Error("method not implemented");
   }
 }
