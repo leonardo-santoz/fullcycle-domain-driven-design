609f051133284f22c21fd3c092637d7c783806fe
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2025-10-08 20:40:47 -0300

    feat: Domain Events - add customer created domain events

---
+++ src/domain/event/customer/handler/send-second-email-when-customer-is-created.handler copy.ts
@@ -0,0 +1,11 @@
+import EventHandlerInterface from "../../@shared/event-handler.interface";
+import EventInterface from "../../@shared/event.interface";
+import CustomerCreatedEvent from "../customer-created-event";
+
+export default class SendSecondEmailWhenCustomerIsCreated
+  implements EventHandlerInterface<CustomerCreatedEvent>
+{
+  handle(event: EventInterface): void {
+    console.log("Esse é o segundo console.log do evento: CustomerCreated");
+  }
+}
