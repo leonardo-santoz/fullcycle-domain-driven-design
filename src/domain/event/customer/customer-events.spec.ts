d963995969509516d68727527d11692df0523eb7
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2025-10-08 22:30:51 -0300

    feat: Domain Events - implement customer address updated event and related tests

--- src/domain/event/customer/customer-events.spec.ts
+++ src/domain/event/customer/customer-events.spec.ts
@@ -3,8 +3,9 @@ import Customer from "../../entity/customer";
 import EventDispatcher from "../@shared/event-dispatcher";
 import CustomerCreatedEvent from "./customer-created-event";
 import SendFirstEmailWhenCustomerIsCreated from "./handler/send-first-email-when-customer-is-created.handler";
-import SendEmailWhenCustomerAddressIsUpdated from './handler/send-email-when-customer-address-is-updated.handler';
-import SendSecondEmailWhenCustomerIsCreated from "./handler/send-second-email-when-customer-is-created.handler copy";
+import SendEmailWhenCustomerAddressIsUpdated from "./handler/send-email-when-customer-address-is-updated.handler";
+import SendSecondEmailWhenCustomerIsCreated from "./handler/send-second-email-when-customer-is-created.handler";
+import CustomerAddressUpdatedEvent from './customer-address-updated-event';
 
 describe("Customer Domain Events", () => {
   it("should send first email when customer is created", () => {
@@ -37,23 +38,26 @@ describe("Customer Domain Events", () => {
     expect(spyEventHandler).toHaveBeenCalled();
   });
 
-  it("should send mail when customer address is updated", () => {
+  it("should send mail when customer address is updated", async () => {
     const eventDispatcher = new EventDispatcher();
     const eventHandler = new SendEmailWhenCustomerAddressIsUpdated();
     const spyEventHandler = jest.spyOn(eventHandler, "handle");
 
-    eventDispatcher.register("CustomerCreatedEvent", eventHandler);
+    eventDispatcher.register("CustomerAddressUpdatedEvent", eventHandler);
 
     const customer = new Customer("1", "John Doe");
-    const address = new Address("Street 1", 1, "1234567890", "São Paulo");
-    customer.Address = address;
 
-    const newCustomerAddress = new Address("Street 2", 2, "1234567810", "São Paulo");
-    customer.changeAddress(newCustomerAddress);
+    const customerAddress = new Address(
+      "Street 2",
+      2,
+      "1234567810",
+      "São Paulo"
+    );
 
-    const customerCreatedEvent = new CustomerCreatedEvent(customer);
+    customer.changeAddress(customerAddress);
 
-    eventDispatcher.notify(customerCreatedEvent);
+    const event = new CustomerAddressUpdatedEvent(customer);
+    eventDispatcher.notify(event);
 
     expect(spyEventHandler).toHaveBeenCalled();
   });
